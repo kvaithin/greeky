8a7d3d1771b86ec3dd1bf906787d93ff37524a0a
Karthik Vaithin <karthik.vaithin@aexp.com>
2023-05-04 21:26:39 +0100

    swich to aura db

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -2,9 +2,11 @@ import neo4j, { Driver, Session } from 'neo4j-driver';
 import {
   NEO4J_USERNAME,
   NEO4J_PASSWORD,
+  NEO4J_INSTANCE_ID,
 } from "@/utils/constants";
 
-const url = 'bolt://localhost:7687';
+// const url = 'bolt://localhost:7687';
+const url = `neo4j+s://${NEO4J_INSTANCE_ID}.databases.neo4j.io`;
 
 const driver: Driver = neo4j.driver(
   url, neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD),
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,11 @@
 const {
   NEO4J_USERNAME = '',
   NEO4J_PASSWORD = '',
+  NEO4J_INSTANCE_ID = '',
 } = process.env;
 
 export {
   NEO4J_USERNAME,
   NEO4J_PASSWORD,
+  NEO4J_INSTANCE_ID,
 }

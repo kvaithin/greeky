8c5d48fcd1e7e03ee67a8848ef510451cf3c168f
Karthik Vaithin <karthik.vaithin@aexp.com>
2023-05-09 22:56:59 +0100

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import GodContainer from "@/components/GodContainer";
 import Toast from "@/components/Toast";
 import GraphHandler from "@/components/GraphHandler";
 import SearchDropDown from "@/components/SearchDropdown";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
+      {/*<Hero />*/}
       <div className="mt-6 mx-auto px-4 max-w-lg flex gap-2">
         <SearchBar isMainNode={true} placeholder='Find Greek Gods...' />
         <SearchBar isShortestPathNode={true} placeholder='Shortest Path...' />
